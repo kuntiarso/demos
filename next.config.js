35baa8dae225495836d5ba04b241682cbba05f35
kuntiarso <gilang.grk17@gmail.com>
2021-10-27 10:42:49 +0700

    update deploy

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 // module.exports = {
 //   reactStrictMode: true,
-// }
-// module.exports = {
-//   images: {
-//     loader: "imgix",
-//     path: "https://noop/",
-//   },
-// }
 
 module.exports = {
   target: 'serverless',
-  reactStrictMode: true,
 };
